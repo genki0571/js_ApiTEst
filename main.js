68cf73ccc92497d8808852850fa974c3c31204ba
なとりーむ <gen71ki05@icloud.com>
2024-11-19 12:08:02 +0900

    APIKEY周辺

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ import 'https://deno.land/std/dotenv/load.ts';
 
 /* サーバーにアクセス（fetch）があったら呼び出される関数 */
 Deno.serve(async (req) => {  
-  const apiKey = Deno.env.get('API_KEY'); // 環境変数の読み込み
+  //const apiKey = Deno.env.get('API_KEY');
+  // 環境変数の読み込み
+  apiKeyGoogle =  Deno.env.get('GOOGLE_API_KEY');
+  apiKeyDeepL =  Deno.env.get('DEEPL_API_KEY');
   console.log(apiKey); // ここはVSCodeのターミナル
 
   // メッセージボディを構成
