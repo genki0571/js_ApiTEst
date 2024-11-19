6a1f40ec195c1f4cad6ea0bb586e9fdfb893bd26
なとりーむ <gen71ki05@icloud.com>
2024-11-19 11:27:00 +0900

    deno追加

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 /* サーバーに接続する非同期関数 */
 async function getResource() {
   // クライアントサイドからサーバーサイドへはfetchで接続する
-  const res = await fetch('http://localhost:8000/');
+  const res = await fetch('https://apitest-1119.deno.dev/');
   const obj = await res.json();
   console.log(obj); // ここはブラウザーの開発者ツールのコンソール
   document.querySelector('h1').textContent = obj.env;
